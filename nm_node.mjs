240846f59f0c62ec1a13c8fe5a24a9dca566ed21
guest271314 <guest271314@gmail.com>
2022-10-12 21:12:53 -0700

    Use .mjs extension, Ecmascript Module import

--- nm_node.mjs
+++ nm_node.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S /path/to/node --max-old-space-size=6 --jitless --expose-gc --v8-pool-size=1
 // Node.js Native Messaging host
 // guest271314, 10-9-2022
+import {openSync, readSync} from 'node:fs';
 // Node.js Native Messaging host constantly increases RSS during usage
 // https://github.com/nodejs/node/issues/43654
 process.env.UV_THREADPOOL_SIZE = 1;
@@ -8,8 +9,6 @@ process.env.UV_THREADPOOL_SIZE = 1;
 // https://github.com/nodejs/node/issues/6456
 // https://github.com/nodejs/node/issues/11568#issuecomment-282765300
 process.stdout._handle.setBlocking(true);
-
-const fs = require('node:fs');
 
 function getMessage() {
   const header = new Uint32Array(1);
@@ -21,12 +20,9 @@ function getMessage() {
 }
 
 function sendMessage(json) {
-  let header = Uint32Array.from(
-    {
-      length: 4,
-    },
-    (_, index) => (json.length >> (index * 8)) & 0xff
-  );
+  let header = Uint32Array.from({
+    length: 4,
+  }, (_,index)=>(json.length >> (index * 8)) & 0xff);
   let output = new Uint8Array(header.length + json.length);
   output.set(header, 0);
   output.set(json, 4);
