1d3609b8ee809542b7fb709e1b3ded486947b18c
guest271314 <guest271314@gmail.com>
2022-10-22 21:43:50 -0700

    Set port globally, handle runtime.lastError

--- background.js
+++ background.js
@@ -1,4 +1,10 @@
-var port = chrome.runtime.connectNative('nm_nodejs');
-port.onMessage.addListener(e=> console.log(e));
-port.onDisconnect.addListener(e=> console.log(e));
-port.postMessage(new Array(20000));
+globalThis.name = chrome.runtime.getManifest().short_name;
+
+globalThis.port = chrome.runtime.connectNative(globalThis.name);
+port.onMessage.addListener((message) => console.log(message));
+port.onDisconnect.addListener((p) => console.log(chrome.runtime.lastError));
+port.postMessage(new Array(200000));
+
+chrome.runtime.onInstalled.addListener((reason) => {
+  console.log(reason);
+});
