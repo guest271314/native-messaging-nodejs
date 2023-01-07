573686f6d822316a2a6657c6357e211cd4051d98
guest271314 <guest271314@gmail.com>
2023-01-06 21:02:10 -0800

    Pass 1024*1024 length message

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ globalThis.name = chrome.runtime.getManifest().short_name;
 globalThis.port = chrome.runtime.connectNative(globalThis.name);
 port.onMessage.addListener((message) => console.log(message));
 port.onDisconnect.addListener((p) => console.log(chrome.runtime.lastError));
-port.postMessage(new Array(200000));
+port.postMessage(new Array(209715));
 
 chrome.runtime.onInstalled.addListener((reason) => {
   console.log(reason);
