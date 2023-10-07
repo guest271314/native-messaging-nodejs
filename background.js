ba2fb348afc8fc332c99e543a8fc39b8c97e3995
guest271314 <guest271314@gmail.com>
2023-10-07 14:41:06 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,30 @@
-globalThis.name = chrome.runtime.getManifest().short_name;
-
-globalThis.port = chrome.runtime.connectNative(globalThis.name);
-port.onMessage.addListener((message) => console.log(message));
-port.onDisconnect.addListener((p) => console.log(chrome.runtime.lastError));
-port.postMessage(new Array(209715));
-
 chrome.runtime.onInstalled.addListener((reason) => {
   console.log(reason);
+});
+
+globalThis.port = null;
+globalThis.externallyConnectablePort = null;
+
+chrome.runtime.onConnectExternal.addListener((externalConnectablePort) => {
+  globalThis.name = chrome.runtime.getManifest().short_name;
+  globalThis.externalPort = externalConnectablePort;
+  globalThis.port = chrome.runtime.connectNative(globalThis.name);
+  port.onMessage.addListener((message) => {
+    globalThis.externalPort.postMessage(message);
+  });
+  port.onDisconnect.addListener((message) => {
+    if (chrome.runtime.lastError) {
+      console.log(chrome.runtime.lastError);
+    };
+    externalPort.disconnect();
+    chrome.runtime.reload();
+  });
+  globalThis.externalPort.onMessage.addListener(async (message) => {
+    port.postMessage(message);
+  });
+  globalThis.externalPort.onDisconnect.addListener(async (e) => {
+    console.log(e);
+    port.disconnect();
+    chrome.runtime.reload();
+  });
 });
