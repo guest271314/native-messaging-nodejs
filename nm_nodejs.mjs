e3154b47e8b5cec9f48d8e5478d78f52c11abf17
guest271314 <guest271314@gmail.com>
2023-10-07 19:44:37 -0700

    Update nm_nodejs.mjs
    Process stdin asynchronously

--- nm_nodejs.mjs
+++ nm_nodejs.mjs
@@ -1,28 +1,38 @@
 #!/usr/bin/env -S ./node --max-old-space-size=6 --jitless --expose-gc --v8-pool-size=1
 // Node.js Native Messaging host
 // guest271314, 10-9-2022
-import {readSync} from 'node:fs';
+import {open} from 'node:fs/promises';
 // Node.js Native Messaging host constantly increases RSS during usage
 // https://github.com/nodejs/node/issues/43654
 process.env.UV_THREADPOOL_SIZE = 1;
 // Process greater than 65535 length input
 // https://github.com/nodejs/node/issues/6456
 // https://github.com/nodejs/node/issues/11568#issuecomment-282765300
+// https://www.reddit.com/r/node/comments/172fg10/comment/k3xcax5/
 process.stdout._handle.setBlocking(true);
 // https://github.com/denoland/deno/discussions/17236#discussioncomment-4566134
-function readFullSync(fd, buf) {
-  let offset = 0;
-  while (offset < buf.byteLength) {
-    offset += readSync(fd, buf, { offset });
+async function readFullAsync(length) {
+  const buffer = new Uint8Array(65536);
+  const data = [];
+  while (data.length < length) {
+    const input = await open("/dev/stdin");
+    let { bytesRead } = await input.read({
+      buffer
+    });
+    if (bytesRead === 0) {
+      break;
+    }
+    data.push(...buffer.subarray(0, bytesRead));
+    await input.close();
   }
-  return buf;
+  return new Uint8Array(data);
 }
 
 function getMessage() {
   const header = new Uint32Array(1);
-  readFullSync(0, header);
+  readFullAsync(0, header);
   const content = new Uint8Array(header[0]);
-  readFullSync(0, content);
+  readFullAsync(0, content);
   return content;
 }
 
