62e8f0e357682a2d1507d08b0ceac2d3111c9546
guest271314 <guest271314@gmail.com>
2024-08-31 15:49:53 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,4 @@
-console.log(chrome.runtime.getManifest().short_name);
+chrome.runtime.onInstalled.addListener(async (reason) => {
+  await chrome.sidePanel.setOptions({ path: "controller.html", enabled: true });
+  await chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(console.error);
+});
