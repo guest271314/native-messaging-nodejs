2288d579480656b789227df1fd6ab4ed2267d52b
guest271314 <guest271314@gmail.com>
2024-08-31 15:41:40 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,1 @@
-globalThis.name = chrome.runtime.getManifest().short_name;
-
-globalThis.port = chrome.runtime.connectNative(globalThis.name);
-port.onMessage.addListener((message) => console.log(message));
-port.onDisconnect.addListener((p) => console.log(chrome.runtime.lastError));
-port.postMessage(new Array(209715));
-
-chrome.runtime.onInstalled.addListener((reason) => {
-  console.log(reason);
-});
+console.log(chrome.runtime.getManifest().short_name);
